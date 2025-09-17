23602c96064fe653e534daed93680dedd4cb82c5
Vriti62 <136335873+Vriti62@users.noreply.github.com>
2025-09-17 11:32:29 +0530

    Added deleting the saved projects functionality  (#1215)
    * added-saving-project-functionality-and-auto-creation-of-collection
    
    * fixed-some-bugs
    
    * added-create-collection-manually-and-fixed-the-same
    
    * added a default-collection collection to directly save if no collection_name
    
    * added-delete-projects-functionality
    
    * fixed-a-minor-issue
    
    * extracted collection and project id from request body instead of params
    
    * updated controller- removed logs

--- backend/Controllers/collection.controller.js
+++ backend/Controllers/collection.controller.js
@@ -1,8 +1,11 @@
 import Collection from "../Models/collection.model.js";
+import Project from "../Models/project.model.js";
 import User from "../Models/user.model.js";
+import mongoose from "mongoose";
 
 /*create a new collection-
  1.in case of manula creation of collection, the project_id is set to null since nothing is saved */ 
+
 export const createNewCollection = async(req,res)=>{
     try{
         const userID = req.user;
@@ -68,7 +71,6 @@ export const saveProject = async (req, res) => {
       );
     }
 
-
     // Case 3: No empty slots → create new document for the project being saved 
     const newDoc = new Collection(
       { 
@@ -88,5 +90,27 @@ export const saveProject = async (req, res) => {
     console.log(err);
     return res.status(400).json(err);
     }
+}
+
+export const deleteProject = async(req,res)=>{
+  try{
+    const userID = req.user;
+    const {collectionID,projectID} = req.body;
+    const existingUser = await User.findById(userID);
+    if(!existingUser)  return res.status(404).json("User not found");
+    const deletedProject = await Collection.findOne({
+      userID:userID,
+      _id:collectionID,
+      project_id:projectID
+    })
+
+    if(!deletedProject) return res.status(404).json("Project not found in this collection");
+
+    await Collection.deleteOne(deletedProject);
+    return res.status(200).json("Project deleted successfully");
+  }catch(err){
+    console.log(err);
+    return res.status(400).json(err);
+  }
 }
 
--- backend/Routes/api/collections.routes.js
+++ backend/Routes/api/collections.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createNewCollection, saveProject} from '../../Controllers/collection.controller.js';
+import { createNewCollection, deleteProject, saveProject} from '../../Controllers/collection.controller.js';
 import { authenticateUser } from '../../Middlewares/auth.middleware.js';
 const collectionRoutes = express.Router();
 collectionRoutes.post("/create-collection", authenticateUser, createNewCollection);
 collectionRoutes.post("/:id",  authenticateUser, saveProject);
+collectionRoutes.delete("/saved-projects", authenticateUser, deleteProject);
 
 
 export default collectionRoutes;
-
