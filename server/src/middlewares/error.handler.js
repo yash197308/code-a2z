ee5b79ff4b8aec9beaf9aa2ec29ca417a7be54be
Avdhesh-Varshney <avdhesh7v@gmail.com>
2025-09-28 20:46:03 +0530

    fix err-handler

--- server/src/middlewares/error.handler.js
+++ server/src/middlewares/error.handler.js
@@ -1,7 +1,10 @@
 import { sendResponse } from '../utils/response.js';
 
-const errorHandler = (err, _req, res) => {
+const errorHandler = (err, _req, res, _next) => {
   console.error(err.stack);
+  if (res.headersSent) {
+    return _next(err);
+  }
   return sendResponse(
     res,
     500,
