a27d4dfba67762bd46458cab0dc57f0439f51236
Avdhesh-Varshney <avdhesh7v@gmail.com>
2025-09-28 15:34:51 +0530

    delete frontend

--- frontend/src/components/NoData.jsx
+++
@@ -1,9 +0,0 @@
-const NoDataMessage = ({ message }) => {
-    return (
-        <div className="text-center w-full p-4 rounded-full bg-gray-50 dark:bg-[#1a1a1a] text-gray-800 dark:text-gray-200 mt-4">
-            <p>{message}</p>
-        </div>
-    )
-}
-
-export default NoDataMessage;
