e1316151bc8512338dc2f675b87a85cd005c1abf
Avdhesh-Varshney <avdhesh7v@gmail.com>
2025-10-11 14:54:02 +0530

    create logout route

--- server/src/routes/api/auth.routes.js
+++ server/src/routes/api/auth.routes.js
@@ -6,12 +6,14 @@ import signup from '../../controllers/auth/signup.js';
 import login from '../../controllers/auth/login.js';
 import changePassword from '../../controllers/auth/change-password.js';
 import refresh from '../../controllers/auth/refresh.js';
+import logout from '../../controllers/auth/logout.js';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/signup', signup);
 authRoutes.post('/login', login);
 authRoutes.get('/refresh', refresh);
+authRoutes.get('/logout', logout);
 authRoutes.patch('/change-password', authenticateUser, changePassword);
 
 export default authRoutes;
