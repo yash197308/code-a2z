9104a8cea165b12c7afaebd5b6f3d6d0c3d47403
Avdhesh-Varshney <avdhesh7v@gmail.com>
2025-10-11 14:46:24 +0530

    create refresh token route

--- server/src/routes/api/auth.routes.js
+++ server/src/routes/api/auth.routes.js
@@ -5,11 +5,13 @@ import authenticateUser from '../../middlewares/auth.middleware.js';
 import signup from '../../controllers/auth/signup.js';
 import login from '../../controllers/auth/login.js';
 import changePassword from '../../controllers/auth/change-password.js';
+import refresh from '../../controllers/auth/refresh.js';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/signup', signup);
 authRoutes.post('/login', login);
+authRoutes.get('/refresh', refresh);
 authRoutes.patch('/change-password', authenticateUser, changePassword);
 
 export default authRoutes;
